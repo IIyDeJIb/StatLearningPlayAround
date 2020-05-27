feeb3b5515a8577bd3aeca0ed97b480db014cc78
IIyDeJIb <kergedancerealternate%!#8008>
2020-05-27 02:06:22 -0500

    first commit

--- Ch4_ex10.py
+++ Ch4_ex10.py
@@ -18,9 +18,23 @@ y = data['Direction']
 data['Direction'] = data['Direction'].map({'Up': 1, 'Down': 0})
 logref_sk = LogisticRegression().fit(X, y)
 y_pred = logref_sk.predict(X)
-confusion_matrix(y, y_pred)
+confMat = confusion_matrix(y, y_pred)
+
 
 # Fit with statsmodels to see R-like summary
 logreg_sm = smf.logit('Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume',
 					  data=data).fit()
 logreg_sm.summary()
+
+#--- Use just Lag2, split train and tests
+y_train = data.loc[data['Year']<2009, 'Direction'].map({'Up':1, 'Down':0})
+y_test = data.loc[data['Year']>=2009, 'Direction'].map({'Up':1, 'Down':0})
+X_train = pd.DataFrame(data.loc[data['Year']<2009,'Lag2'])
+X_test = pd.DataFrame(data.loc[data['Year']>=2009,'Lag2'])
+
+logreg_Lag2 = LogisticRegression().fit(X_train, y_train.values.ravel())
+
+y_pred = logreg_Lag2.predict(X_test)
+confusion_matrix(y_test, y_pred)
+
+
